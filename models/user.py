1c109405f1c295bad0677b9e505be222cf6e7a94
Godsway <gasamoah@wacci.ug.edu.gh>
2024-03-08 17:03:38 -0800

    My user commit

---
+++ models/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Authors: Godsway and Prosper
+
+"""Defines a class User"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Represent a User."""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new User"""
+
+        super().__init__(*args, **kwargs)
