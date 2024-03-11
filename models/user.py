e2f5ce4f08473421aa90a8064727ec76bde164b0
Prosper Atu <atu.prosper@gmail.com>
2024-03-11 13:22:15 +0100

    task 8 - created user.py

--- models/user.py
+++ models/user.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-# Authors: Godsway and Prosper
-
-"""Defines a class User"""
+"""Defines a class User that inherits from BaseModel"""
 
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represent a User."""
-
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    """User class that inherits from BaseModel"""
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new User"""
+        """
+        Initializes User instance
 
+        Public class attributes:
+            email (str): The email of the user.
+            password (str): The password of the user.
+            first_name (str): The first name of the user.
+            last_name (str): The last name of the user.
+        """
         super().__init__(*args, **kwargs)
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
