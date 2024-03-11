d9f3dc33873916c3b6810828fc2c2a8095158adb
Prosper Atu <atu.prosper@gmail.com>
2024-03-11 11:00:49 +0100

    task 6 - Created the console methods

--- console.py
+++ console.py
@@ -1,2 +1,32 @@
 #!/usr/bin/python3
-# Authors: Godsway and Prosper
+"""
+Defines HBNBCommand class
+This module contains the entry point of the command interpreter
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """The entry point of the command interpreter"""
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        print("")
+        return True
+
+    def emptyline(self):
+        """
+        Called when an empty line is entered
+          - Do nothing
+        """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
