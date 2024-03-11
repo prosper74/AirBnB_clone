9bb8021f24a13a8315292a81f365973ca98b2e57
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-03-11 14:12:50 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -7,18 +7,7 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """User class that inherits from BaseModel"""
 
-    def __init__(self, *args, **kwargs):
-        """
-        Initializes User instance
-
-        Public class attributes:
-            email (str): The email of the user.
-            password (str): The password of the user.
-            first_name (str): The first name of the user.
-            last_name (str): The last name of the user.
-        """
-        super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+    self.email = ""
+    self.password = ""
+    self.first_name = ""
+    self.last_name = ""
